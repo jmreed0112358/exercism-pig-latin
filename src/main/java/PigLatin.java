d2d7838fe5f713ffbccaa482fb0b009d24eca6a3
Jeremy M. Reed <reeje76@gmail.com>
2016-06-05 22:34:08 -0600

    More stubs.

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -8,6 +8,36 @@ public class PigLatin
 	}
 
 	public static String translate(String input) {
+		// Check for empty string.
+		// Reduce all characters to lower case.
+		// Tokenize string.
+		// For each word in the string.
+			// If the word is 3 chars or longer.
+				// nQU check.
+				// Thr check.
+				// Sch check.
+		
+			// If the word is 2 chars or longer.
+			// If the word is 1 char or longer.
 		return "Foo";
+	}
+	
+	/**
+	 * Checks to see if the given 3 letter chunk matches the nQU rule, where n is a consonant, followed by the letters qu.
+	 * This chunk is treated as a single consonant.
+	 * @param chunk 3 letter chunk to be checked.
+	 * @return
+	 */
+	private boolean nquRuleCheck(String chunk) {
+		return false;
+	}
+	
+	/**
+	 * Applies the nqu rule, we treat this like a consonant.
+	 * @param word
+	 * @return
+	 */
+	private String applyNquRule(String word) {
+		return "";
 	}
 }
