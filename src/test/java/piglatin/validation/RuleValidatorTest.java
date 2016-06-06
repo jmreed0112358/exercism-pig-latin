e35d4c5a58437eaa3e368112dcdc728569728f23
Jeremy M. Reed <reeje76@gmail.com>
2016-06-06 01:54:09 -0600

    Unit tests for RuleValidator
    Tests for isVowel and isConsonant methods.

--- src/test/java/piglatin/validation/RuleValidatorTest.java
+++ src/test/java/piglatin/validation/RuleValidatorTest.java
@@ -8,21 +8,37 @@ import org.junit.Test;
 
 public class RuleValidatorTest
 {
-
+	private RuleValidator ruleValidator = null;
+	
 	@Before
 	public void setUp( ) throws Exception
 	{
 	}
 
-	@After
-	public void tearDown( ) throws Exception
-	{
-	}
-
+	// isVowel tests.
+	
 	@Test
-	public void test( )
-	{
-		fail( "Not yet implemented" );
+	public void test_isVowel_VowelChunk_ReturnsTrue() {
+		this.ruleValidator = new RuleValidator();
+		assertTrue(this.ruleValidator.isVowel( 'a' ));
+	}
+	
+	@Test
+	public void test_isVowel_ConsonantChunk_ReturnsFalse() {
+		this.ruleValidator = new RuleValidator();
+		assertFalse(this.ruleValidator.isVowel( 'z' ));
 	}
 
+	// isConsonant tests.
+	@Test
+	public void test_isConsonant_ConsonantChunk_ReturnsTrue() {
+		this.ruleValidator = new RuleValidator();
+		assertTrue(this.ruleValidator.isConsonant( 'z' ));
+	}
+	
+	@Test
+	public void test_isConsonant_VowelChunk_ReturnsFalse() {
+		this.ruleValidator = new RuleValidator();
+		assertFalse(this.ruleValidator.isConsonant( 'a' ));
+	}
 }
